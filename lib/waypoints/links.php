033d6e2c6d1e044c4aa776be69d48e07bfffc7b7
castro876 <jovmrt@gmail.com>
2024-08-20 17:28:40 -0500

    business website first commit

---
+++ lib/waypoints/links.php
@@ -0,0 +1,45 @@
+Global function:
+
+Some examples of built-in global functions in JavaScript include: parseInt() : Converts a string to an integer. parseFloat() : Converts a string to a floating-point number. isNaN() : Checks if a value is Not-a-Number (NaN).
+
+radix: 
+
+A radix parameter specifies the number system to use: 2 = binary, 8 = octal, 10 = decimal, 16 = hexadecimal. If radix is omitted, JavaScript assumes radix 10. If the value begins with "0x", JavaScript assumes radix 16.
+
+
+Aumber system:
+A system for representing the counting numbers. These include: The usual "base ten" or "decimal" system: 1, 2, 3, … , 10, 11, 12, … Binary numbers are represented on the number system with '1s' and '0'
+ 
+constructor:
+
+ Constructor function for Person objects
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+
+const myFather = new Person("John", "Doe", 50, "blue")
+
+
+Methods for object:
+
+The call() method is a predefined JavaScript method.
+It can be used to invoke (call) a method with an owner object as an argument (parameter).
+Used to call an object anywhere.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
